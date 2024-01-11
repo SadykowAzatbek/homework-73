4093239f51f1ee948cd20012b38065c511ec674a
SadykowAzatbek <sadykowazatbek@gmail.com>
2024-01-11 16:47:03 +0600

    Теперь при get запросе на /encode:text будет выводится зашифрованный код текста. При запросе на /decode:text при этом ввести зашифрованный код, то выводит исходный текст написанный в /encode:text.

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const app = express();
+const Vigenere = require('caesar-salad').Vigenere;
 
 const port = 8000;
+const password = 'password';
 
-app.get('/:text', (req, res) => {
-  res.send(req.params.text);
+app.get('/text/:word', (req, res) => {
+  res.send(req.params.word);
+});
+
+app.get('/encode/:text', (req, res) => {
+  res.send(Vigenere.Cipher(password).crypt(req.params.text));
+});
+
+app.get('/decode/:text', (req, res) => {
+  res.send(Vigenere.Decipher(password).crypt(req.params.text));
 });
 
 app.listen(port, () => {
